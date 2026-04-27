2eb14f3a5a15633797af3375eda4e05794f2f913
saadali <saadali6991550@gmail.com>
2026-04-27 19:36:31 +0500

    5-Routing

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default  function About() {
+  return (
+    <div>this is about page</div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <h4>hello welcome </h4>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -48,7 +49,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Deploy Now
+            Deploy Now on 
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
