2572b9bd1933ee957666590505f066035e247ed9
saadali <saadali6991550@gmail.com>
2026-06-13 23:41:18 +0500

    37-handle post request

--- app/Comments/route.tsx
+++ app/Comments/route.tsx
@@ -1,4 +1,20 @@
+import { json } from "stream/consumers";
 import { Comment } from "./data";
 export async function GET(){
     return Response.json(Comment)
+}
+export async function POST(request: Request){
+   const comm= await request.json()
+   const newComment={
+    id:Comment.length +1,
+    name: comm.name
+   }
+   Comment.push(newComment)
+   return new Response(JSON.stringify(newComment),{
+    headers:{
+        "Content-Type": "application/json",
+    
+    },
+    status: 201
+   })
 }
