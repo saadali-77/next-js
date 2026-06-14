a0430ae45d78239079a942bc338c6611ba7fa4d3
saadali <saadali6991550@gmail.com>
2026-06-14 19:14:06 +0500

    39- handle patch request

--- app/Comments/[id]/route.tsx
+++ app/Comments/[id]/route.tsx
@@ -7,3 +7,15 @@ export async function GET(req:Request,{params}:{
     return Response.json(comment)
 }
 
+export async function PATCH(req:Request,{params}:{
+    params:Promise<{id:string}>
+}
+){
+    const {id}= await params;
+    const comm= await req.json()
+    const commentIndex = Comment.findIndex((com)=>com.id===parseInt(id))
+    Comment[commentIndex].name = comm.name
+    return Response.json(Comment[commentIndex])
+
+}
+
