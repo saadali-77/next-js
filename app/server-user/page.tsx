f763c9c5db2e0ad97208a9cbf1e1d036d9bff107
saadali <saadali6991550@gmail.com>
2026-06-15 20:13:19 +0500

    69-handle loading and error

--- app/server-user/page.tsx
+++ app/server-user/page.tsx
@@ -5,7 +5,10 @@ type User={
     phone:string
 }
 export default async function Server(){
-  const res=  await fetch('https://jsonplaceholder.typicode.com/users')
+  const res=  await fetch('https://jsonplaceholder.typicode.com/usersrr')
+
+  if(!res.ok) throw new Error('failed to fetch data')
+
 const users :User[]= await res.json()
 console.log(users)
 }
